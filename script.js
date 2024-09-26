54b616ed931a6d966b0fc6b34d03e04c2bb3f234
Ozheim <heikel.zouaoui@gmail.com>
2024-09-26 18:44:49 +0200

    Refactor: created 'assets' and 'style' directories for better organization
    Added intro animation effect

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const onOrOff = () => {
-  const li = document.querySelectorAll("li");
+const onOrOff = (event) => {
+  const li = event.currentTarget;
+  li.classList.remove("layers", "glitch", "font");
 };
+
+const onMouseOut = (event) => {
+  const li = event.currentTarget;
+  li.classList.add("layers", "glitch", "font");
+};
+
+const loading = () => {
+  const p = document.getElementById("loading");
+  let res = "";
+
+  if (p) {
+    const interval = setInterval(() => {
+      res += ".";
+      if (res.length > 3) {
+        return;
+      }
+      p.textContent = "Loading" + res;
+    }, 500);
+  }
+};
+
+loading();
