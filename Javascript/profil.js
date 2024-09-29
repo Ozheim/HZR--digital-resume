d5ebb258955e3f89a1d61ed56227d92869593a08
Ozheim <heikel.zouaoui@gmail.com>
2024-09-29 15:13:31 +0200

    Project page desktop finished Refactoring: JavaScript script added to the directory

---
+++ Javascript/profil.js
@@ -0,0 +1,21 @@
+const mainDiv = document.querySelector(".main-div");
+const video = document.querySelector(".video-background");
+const link = document.querySelector(".back");
+const loadingElement = document.getElementById("loading");
+
+link.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  video.src = "../assets/video-reverse.mp4";
+  video.classList.add("reverse");
+  video.load();
+
+  video.playbackRate = 2;
+  video.play();
+
+  mainDiv.classList.add("shrink");
+
+  setTimeout(() => {
+    window.location.href = "../index.html";
+  }, 2000);
+});
