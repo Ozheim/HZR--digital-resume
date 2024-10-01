b722ebe82290bd0185dc4527ae7a88f8750a6280
Ozheim <heikel.zouaoui@gmail.com>
2024-10-01 23:12:20 +0200

    skill page functionnality added, responsive design for this page added

---
+++ Javascript/competence.js
@@ -0,0 +1,25 @@
+async function chargerCompetences() {
+  const response = await fetch("../staticBackend/technologies.json");
+  const data = await response.json();
+  return data.competences;
+}
+
+const afficherDescription = (competence) => {
+  const descriptionDiv = document.querySelector(".description");
+
+  descriptionDiv.innerHTML = `
+        <h3>${competence.nom}</h3>
+        <p>Niveau : ${competence.niveau}</p>
+        <p>${competence.description}</p>
+    `;
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const competences = await chargerCompetences();
+
+  document.querySelectorAll(".skills-logo").forEach((logo, index) => {
+    logo.addEventListener("click", () => {
+      afficherDescription(competences[index]);
+    });
+  });
+});
